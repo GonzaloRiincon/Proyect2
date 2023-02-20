30f4b9f3f0c94ca6fd5525491aeadfcf7db30f3d
Gonzalo Rincón Panadero <gonzalorinconp1@gmail.com>
2023-02-20 17:06:15 +0100

    d

--- services/drivers.service.js
+++ services/drivers.service.js
@@ -15,28 +15,41 @@ class ApiService {
         return this.api.get(`/${year}/drivers.json`)
     }
     getOneDriver(driver) {
-        const promises = [this.api.get(`/drivers/${driver}.json`), this.api.get(`/drivers/${driver}/constructors.json`), this.api.get(`/drivers/${driver}/driverStandings.json`)]
+        const promises = [this.api.get(`/drivers/${driver}.json`), this.api.get(`/drivers/${driver}/constructors.json`), this.api.get(`/drivers/${driver}/driverStandings.json`), Driver.find({ surname: driver })]
         return Promise
             .all(promises)
-            .then(([driverData, driverConstructor, driverPoints]) => {
+            .then(([driverData, driverConstructor, driverPoints, existing]) => {
                 const { givenName, familyName, dateOfBirth, nationality, } = driverData.data.MRData.DriverTable.Drivers[0]
+                if (!existing) {
+                    return Driver
+                        .create({
+                            name: givenName,
+                            surname: familyName,
+                            birthday: dateOfBirth,
+                            nationality: nationality,
+                            constructors: driverConstructor.data.MRData.ConstructorTable.Constructors[0].name,
+                            points: driverPoints.data.MRData.StandingsTable.StandingsLists
+                                .map(element => element.DriverStandings.map(elm => Number(elm.points)))
+                                .flat(2).reduce((acc, curr) => acc + curr),
+                        })
+                        .then(createdDriver => {
 
-                return Driver
-                    .create({
-                        name: givenName,
-                        surname: familyName,
-                        birthday: dateOfBirth,
-                        nationality: nationality,
-                        constructors: driverConstructor.data.MRData.ConstructorTable.Constructors[0].name,
-                        points: driverPoints.data.MRData.StandingsTable.StandingsLists
-                            .map(element => element.DriverStandings.map(elm => Number(elm.points)))
-                            .flat(2).reduce((acc, curr) => acc + curr),
-                    })
-                    .then(createdDriver => {
+                            return createdDriver
+                        })
+                        .catch(err => console.log(err))
+                }
+                if (existing) {
+                    Driver
+                        .find({ surname: driver })
+                        .then(driverProf => {
+                            return driverProf
+                        })
+                        .catch(err => console.log(err))
+                }
 
-                        return createdDriver
-                    })
             })
+            .catch(err => console.log(err))
+
     }
 
 }
