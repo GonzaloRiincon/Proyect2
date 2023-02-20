4367e2d168e7ae01d79f7b049f4f3261b5ec33fd
“Adrian <johndoe@example.com>
2023-02-20 19:58:30 +0100

    done

--- routes/drivers.routes.js
+++ routes/drivers.routes.js
@@ -15,11 +15,18 @@ router.get('/list', (req, res, next) => {
         .getAllDrivers()
         .then(response => {
             console.log(response.data)
-            res.render('drivers/list', { driver: response.data })
+            res.render('drivers/list', { driver: response })
         })
         .catch(err => next(err))
 })
 
+router.get('/list/year', (req, res, next) => {
+    const { year } = req.query
+    driverService
+        .getAllDriversByYear(year)
+        .then(driverSeason => res.render('drivers/list', { driver: driverSeason }))
+        .catch(err => next(err))
+})
 
 
 router.get('/:driverName', (req, res, next) => {
